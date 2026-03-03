5c8f6b2a6f7eee1badd3116a571d36dc8bb5ed6f
mgarcia-2021129 <mgarcia-2021129@kinal.edu.gt>
2026-03-02 19:01:01 -0600

    refactor: renombrar el dominio todo a task y actualizar las rutas a /api/tasks

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const todoRoutes = require("./routes/todo.routes");
+const taskRoutes = require("./routes/task.routes");
 const errorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/todos", todoRoutes);
+app.use("/api/tasks", taskRoutes);
 
 app.get('/', (req, res) => {
     res.send('API running');
